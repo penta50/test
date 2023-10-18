6f46139c201ed80b046ed1eae565de8c6f72bf8a
penta50 <148294822+penta50@users.noreply.github.com>
2023-10-18 09:25:12 +0900

    Add files via upload

---
+++ makedir.py
@@ -0,0 +1,16 @@
+import datetime
+tod = datetime.date.today()
+strtod = tod.strftime("%Y%m%d")
+
+import os
+
+todfolder = tod.strftime("%Y%m%d")
+
+import os 
+dir  = os.environ['USERPROFILE'] + "\\logfile\\" + todfolder + "\\"
+if not os.path.exists(dir):
+     os.mkdir(dir)
+else:
+     print("フォルダあり")
+
+
